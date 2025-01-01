0928cd5742f95d5ada2c045af54f2439a16a8ed3
almirjgg <almirjgg@gmail.com>
2025-01-01 09:49:22 -0300

    chore: add axios and authslice

---
+++ src/api/calendarApi.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { getEnvVariables } from '../helpers';
+
+const { VITE_API_URL } = getEnvVariables();
+
+export const calendarApi = axios.create({
+  baseURL: VITE_API_URL,
+});
+
+calendarApi.defaults.headers.post['Content-Type'] = 'application/json';
+
+calendarApi.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${import.meta.env.VITE_API_TOKEN}`;
+  return config;
+});
+
+calendarApi.interceptors.response.use(
+  config => {
+    return config;
+  },
+  async error => {
+    if (error.response.status === 401) {
+      console.log('Token no válido');
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default calendarApi;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import { CalendarPage } from '../calendar';
 import { LoginPage } from '../auth';
 
 export const AppRouter = () => {
-  const authStatus = 'authenticated';
+  const authStatus = 'checking';
   return (
     <Routes>
       {authStatus === 'authenticated' ? (
